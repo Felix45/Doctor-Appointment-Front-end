252031718b355d015621b74d52918243f552a83d
Felix Ouma <fatonoh@gmail.com>
2022-11-16 18:24:43 +0300

    Add user profile component

---
+++ src/components/Profile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Container } from 'react-bootstrap';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+function Profile() {
+  const { token } = useSelector((state) => state.token);
+
+  return (
+    <Container className="mt-2">
+      <Row>
+        <Col xs={12}>
+          { token.isLoggedIn && <h4>{ `Hi, ${token.username}!` }</h4> }
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Profile;
