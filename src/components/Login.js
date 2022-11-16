8d29803828baf5f00b33d83cfa8afbc21987ea0c
Felix Ouma <fatonoh@gmail.com>
2022-11-16 18:24:13 +0300

    Add user login process logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import { userSignInThunk } from '../redux/slices/loginSlice';
 
 function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginAttempt, setLoginAttempt] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email !== '' && password !== '') {
+      dispatch(userSignInThunk({ email, password }));
+      setEmail('');
+      setPassword('');
+      setLoginAttempt(true);
+    }
+  };
+
+  const { token } = useSelector((state) => state.token);
+  useEffect(() => {
+    if (token.isLoggedIn) navigate('/');
+  }, [token, navigate]);
+
   return (
-    <h1>Login</h1>
+    <Form className="mt-5" onSubmit={(e) => { handleSubmit(e); }}>
+      <Form.Group className="mb-3">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter your email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Enter your password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+      </Form.Group>
+
+      <Form.Group>
+        <Button type="submit">Sign In</Button>
+      </Form.Group>
+      <Col xs={12} className="mt-2 p-2">
+        <Row>
+          { loginAttempt && <div className="alert alert-danger">Login not successful</div> }
+        </Row>
+      </Col>
+    </Form>
   );
 }
 
