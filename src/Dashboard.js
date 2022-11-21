497fb30afc705de8754b502b76dc3f5e214b7d14
Felix Ouma <fatonoh@gmail.com>
2022-11-22 01:07:18 +0300

    Add fontawsome icons to menu items

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect } from 'react';
+import {
+  FaHome, FaUserPlus, FaSignInAlt, FaSignOutAlt, FaEdit, FaList,
+} from 'react-icons/fa';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
@@ -27,27 +30,89 @@ function Dashboard() {
   };
 
   return (
-    <Container>
+    <Container fluid>
       <Row>
-        <Col xs={3}>
-          <h1>Menu</h1>
+        <Col xs={2} className="menu">
+          <h1 className="text-center my-4">Doctors Appointment</h1>
           <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            { (token.isLoggedIn === undefined || !token.isLoggedIn) && <li><NavLink to="/signup">Sign Up</NavLink></li> }
-            { (token.isLoggedIn === undefined || !token.isLoggedIn) && <li><NavLink to="/login">Sign In</NavLink></li> }
+            <li>
+              <NavLink to="/">
+                <FaHome />
+                {' '}
+                Home
+              </NavLink>
+            </li>
+            { (token.isLoggedIn === undefined || !token.isLoggedIn) && (
+            <li>
+              <NavLink to="/signup">
+                <FaUserPlus />
+                {' '}
+                Sign Up
+              </NavLink>
+            </li>
+            ) }
+            { (token.isLoggedIn === undefined || !token.isLoggedIn) && (
+            <li>
+              <NavLink to="/login">
+                <FaSignInAlt />
+                {' '}
+                Sign In
+              </NavLink>
+            </li>
+            ) }
             { (token.isLoggedIn !== undefined && token.isLoggedIn && token.role === 'admin')
-              && <li><NavLink to="/doctor">Add Doctor</NavLink></li>}
+              && (
+              <li>
+                <NavLink to="/doctor">
+                  <FaEdit />
+                  {' '}
+                  Add Doctor
+                </NavLink>
+              </li>
+              )}
             { (token.isLoggedIn !== undefined && token.isLoggedIn)
-              && <li><NavLink to="/doctors/">Doctors</NavLink></li>}
+              && (
+              <li>
+                <NavLink to="/doctors/">
+                  <FaList />
+                  {' '}
+                  Doctors
+                </NavLink>
+              </li>
+              )}
             { (token.isLoggedIn !== undefined && token.isLoggedIn)
-              && <li><NavLink to="/appointments">Book Appointment</NavLink></li>}
+              && (
+              <li>
+                <NavLink to="/appointments">
+                  <FaEdit />
+                  {' '}
+                  Book Appointment
+                </NavLink>
+              </li>
+              )}
             { (token.isLoggedIn !== undefined && token.isLoggedIn)
-              && <li><NavLink to="/myappointments">My Appointments</NavLink></li>}
+              && (
+              <li>
+                <NavLink to="/myappointments">
+                  <FaList />
+                  {' '}
+                  My Appointments
+                </NavLink>
+              </li>
+              )}
             { (token.isLoggedIn !== undefined && token.isLoggedIn)
-              && <li><NavLink onClick={handleLogOut}>Sign Out</NavLink></li>}
+              && (
+              <li>
+                <NavLink onClick={handleLogOut}>
+                  <FaSignOutAlt />
+                  {' '}
+                  Sign Out
+                </NavLink>
+              </li>
+              )}
           </ul>
         </Col>
-        <Col xs={9}>
+        <Col xs={10}>
           <Profile />
           <Splash />
           <Outlet />
