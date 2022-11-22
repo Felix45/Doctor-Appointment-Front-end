fc27792d49a810c4408568346ec9e295165b55e9
Henry <dokinruss@gmail.com>
2022-11-22 23:17:03 +0000

    Modifty the doctordetails.js

--- src/components/DoctorDetails.js
+++ src/components/DoctorDetails.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 
 const DoctorDetails = () => {
@@ -14,27 +15,50 @@ const DoctorDetails = () => {
 
   return (
     <Container>
-      <Row>
-        <Link to="/doctors">
+      <Row className="mb-3">
+        <Link to="/doctors" className="text-primary">
           <span>
-            <FaArrowLeft />
+            <FaArrowLeft className="mx-2" />
             All Doctors
           </span>
         </Link>
       </Row>
       <Row className="d-flex">
-        <Col xs={12}>
-          <Card>
+        <Col xs={12} md={5}>
+          <Card className="mb-4 p-3">
             <Card.Img variant="top" src={`https://finalcapstonedoctorappointment.herokuapp.com/images/${doctor.photo}`} alt={`Doctor ${doctor.name} photo`} />
-            <Card.Body>
-              <Card.Title>{ doctor.name }</Card.Title>
-              <Card.Title>{ doctor.specialization }</Card.Title>
-              <Card.Text>{ doctor.bio }</Card.Text>
-              <div>
-                <Link to="/doctors"><span><FaArrowLeft /></span></Link>
-              </div>
-            </Card.Body>
           </Card>
+        </Col>
+        <Col xs={12} md={5}>
+          <Card.Body>
+            <Card.Title>{ doctor.name }</Card.Title>
+
+            <div>
+              <Table className="mt-5" striped bordered hover size="sm">
+                <tbody>
+                  <tr>
+                    <th>Speciality</th>
+                    <td>{ doctor.specialization }</td>
+                  </tr>
+                  <tr>
+                    <th>Bio</th>
+                    <td>{ doctor.bio }</td>
+                  </tr>
+                  <tr>
+                    <th>See Dcotor</th>
+                    <td className="d-flex justify-content-end"><Link to="/appointments" className="btn btn-primary">Book Appointment</Link></td>
+                  </tr>
+                </tbody>
+              </Table>
+              <Link to="/doctors" className="text-primary">
+                <span>
+                  <FaArrowLeft />
+                  {' '}
+                  Back
+                </span>
+              </Link>
+            </div>
+          </Card.Body>
         </Col>
       </Row>
     </Container>
